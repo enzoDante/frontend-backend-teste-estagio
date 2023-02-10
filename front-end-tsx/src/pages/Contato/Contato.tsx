2a6999eecc0731159df5a7d24f88b1573808eb52
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2023-02-10 16:40:14 -0300

    Update Contato.tsx

--- front-end-tsx/src/pages/Contato/Contato.tsx
+++ front-end-tsx/src/pages/Contato/Contato.tsx
@@ -4,7 +4,18 @@ export default function Contato(){
 
     return(
         <StyledContato>
-            
+            <form action="" method="post">
+                <div>
+                    <label htmlFor="nome">Nome:</label>
+                    <input type="text" name="nome" id="nome" />
+                    <p>{'erro'}</p>
+                </div>
+                <div>
+                    <label htmlFor="email">E-mail:</label>
+                    <input type="email" name="email" id="email" />
+                    <p>{'erro'}</p>
+                </div>
+            </form>
         </StyledContato>
     )
 }
