705fe37fee70d320258970b1e6857cad1406594b
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2023-02-12 12:03:51 -0300

    adicionando cards

--- front-end-tsx/src/components/Styles/StyledPadrao.tsx
+++ front-end-tsx/src/components/Styles/StyledPadrao.tsx
@@ -9,13 +9,15 @@ export const StyledPadrao = styled.main`
     margin: auto;
     
     border-radius: 0 0 20px 20px;
-    background-color: #f0f0f0;
+    /* background-color: #f0f0f0; */
+    background-image: linear-gradient(135deg, #f0f0f0, #cfdee2);
     box-shadow: 2px 3px 5px black;
 
     h1{
         font-size: 4em;
     }
     p{
+        margin: auto;
         max-width: 1000px;
         text-align: justify;
         text-indent: 1em;
@@ -30,5 +32,68 @@ export const StyledPadrao = styled.main`
     @keyframes paragrafo{
         from {opacity: 0; transform: translateY(50px);}
         to {opacity:1 ; transform: translateY(0px);}
+    }
+
+    #parentGrid{
+        border: 2px solid transparent;
+        margin: 0px -20px -20px -20px;
+        border-radius: 0 0px 20px 20px;
+        /* background-color: #e9faff; */
+        
+        h1{
+            text-align: center;
+            color: #216479;
+        }
+    }
+    section{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        justify-items: center;
+        max-width: 1200px;
+        margin: auto;
+        margin-top: -5vh;
+        margin-bottom: 5vh;
+        padding: 20px;
+
+        fieldset{
+            border: 2px solid #339fc0;
+            max-width: 350px;
+            border-radius: 20px;
+            height: 250px;
+            /* overflow: hidden; */
+            animation: cards 500ms;
+        }
+        legend{
+            margin-left: 2vh;
+        }
+        legend h2{
+            /* border: 2px solid black; */
+            color: #339fc0;
+            font-size: 3em;
+        }
+        p{
+            margin-top: -5vh;
+            font-size: 1.2em;
+            box-shadow: none;
+            background-color: transparent;
+            height: 100px;
+            overflow: hidden;
+            animation: none;
+        }
+
+        @keyframes cards{
+            from {opacity: 0; transform: translateY(40px);}
+            to {opacity:1 ; transform: translateY(0px);}
+        }
+    }
+    @media screen and (max-width: 1200px){
+        section{
+            grid-template-columns: 1fr 1fr;
+        }
+    }
+    @media screen and (max-width: 800px) {
+        section{
+            grid-template-columns: 1fr;
+        }
     }
 `
--- front-end-tsx/src/pages/missao-valores/missaovalores.tsx
+++ front-end-tsx/src/pages/missao-valores/missaovalores.tsx
@@ -11,6 +11,24 @@ export default function Missaovalores(){
 Nossos valores são os princípios de relacionamentos transparentes e respeito a todos clientes e colaboradores.
                 </p>
             </div>
+
+            <div id="parentGrid">
+                <h1>Cards</h1>
+                <section>
+                    <fieldset>
+                        <legend><h2>01</h2></legend>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse animi, eligendi.</p>
+                    </fieldset>
+                    <fieldset>
+                        <legend><h2>02</h2></legend>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo odio culpa dolore a, praesentium similique dolores veniam. Aliquam enim obcaecati eius quasi vel.</p>
+                    </fieldset>
+                    <fieldset>
+                        <legend><h2>03</h2></legend>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus fugit ab in labore deserunt.</p>
+                    </fieldset>
+                </section>
+            </div>
         </StyledPadrao>
     )
 }
