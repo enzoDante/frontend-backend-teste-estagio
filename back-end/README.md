e6adaa7e26a71a8265bcd1b318a22190df7554b5
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2023-02-25 22:08:38 -0300

    finalizando api

---
+++ back-end/README.md
@@ -0,0 +1,7 @@
+#Instruções para uso da api
+#
+##Cadastro
+<p>Para utilizar a rota de cadastro, o usuário deve criar um json com os respectivo valores de cadastro:
+nome, email, telefone e senha!
+
+</p>
--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require("cors")
 const bodyParse = require("body-parser")
 const sb = require('./supabase')
+const jwt = require("jsonwebtoken")
 
 const app = express()
 
@@ -44,17 +45,79 @@ app.post('/api/usuario/cadastro', (req, res) => {
     })
 
 })
+app.post('/api/usuario/login', (req, res) => {
+    const json = req.body.dados
+    const values = JSON.parse(json)
+
+    sb.from('usuarios').select("id, created_at, nome, email, telefone").match({email: values.email, senha: values.senha}).then((e) => {
+        if(e.data[0]  == null){
+            console.log("Usuário inexistente!")
+            res.send(false)
+        }
+        else{
+            const token = jwt.sign({
+                id_usuario: e.data[0].id,
+                nome: e.data[0].nome,
+                email: e.data[0].email
+            }, 'key',
+            {
+                expiresIn: "1h"
+            })
+            console.log(e.data[0])
+            res.send(token)
+        }
+    }).catch((err) => {
+        console.log("Erro ao consultar o banco de dados!")
+        res.send("Erro ao consultar o banco de dados!")
+    })
+})
 
 app.post('/api/usuario/remove', (req, res) => {
-    const id = req.body.id
-    sb.from('usuarios').delete().eq('id', id).then((e) => {
-        console.log(e)
-        console.log('deletado com sucesso')
-        res.send("deletado com sucesso!")
-    }).catch((err) => {
-        console.log(err)
-        res.send("erro ao deletar usuario")
-    })
+    const json = req.body.dados
+    const values = JSON.parse(json)
+
+    try{
+        jwt.verify(values.token, "key")
+
+        const id = values.id
+        sb.from('usuarios').delete().eq('id', id).then((e) => {
+            console.log(e)
+            console.log('deletado com sucesso')
+            res.send("deletado com sucesso!")
+        }).catch((err) => {
+            console.log("Erro ao consultar o banco de dados!")
+            res.send("erro ao deletar usuario")
+        })
+
+    }catch(erro){
+        console.log("falha na verificação do token!")
+        res.send(false)
+    }  
+})
+
+app.post("/api/usuario/update", (req, res) => {
+    const json = req.body.dados
+    const values = JSON.parse(json)
+
+    try{
+        jwt.verify(values.token, "key")
+
+        const updateValues = values.Update 
+        sb.from('usuarios').update({
+            nome: updateValues.nome,
+            email: updateValues.email,
+            telefone: updateValues.telefone,
+            senha: updateValues.senha
+
+        }).eq("id", values.id).then((e) => {
+            console.log("Usuário atualizado com sucesso!")
+            res.send("Usuario atualizado!")
+        })
+
+    }catch(erro){
+        console.log("erro na verificação do token")
+        res.send(false)
+    }
 })
 
 // leitura de dados
